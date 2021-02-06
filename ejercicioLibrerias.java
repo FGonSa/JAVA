a5eb2bf3ed1666df7603fa89a5d536cbc2d7451e
Francisco Javier González <78041280+FGonSa@users.noreply.github.com>
2021-02-07 00:54:55 +0100

    Update ejercicioLibrerias.java

--- ejercicioLibrerias.java
+++ ejercicioLibrerias.java
@@ -1,7 +1,5 @@
 package ejerciciolibrerias;
 
-import library.*;
-
 /**
  *
  * @author Frank.Santos
@@ -15,55 +13,52 @@ public class EjercicioLibrerias {
         boolean arrayPositions[] = new boolean[LENGTHMAX]; //Array aux para la funcion positionsForArray()
         float media = 0; //Media que guardamos del return de calcularMedia()
         int valorMax = 0; //Valor Max del Array que nos devuelve la función ordenarVector()
- 
-        //Función para pedir posiciones al usuario. Le enviamos dos arrays.
-        positionsForArray(array, arrayPositions);
 
-        //Uso de la librería
-        //LLeno el vector con las posiciones solicitadas en la función anterior
+        //Función para pedir posiciones al usuario. Le enviamos dos arrays y cuantas veces debe solicitar posición.
+        positionsForArray(array, arrayPositions, 2);//Por ejemplo, vamos a hacer que nos pida 2 veces posición.
+
+        //-----------------------------------------
+        //DISTINTAS PRUEBAS DEL USO DE LA LIBRERÍA
+        //-----------------------------------------
+        //LLeno el vector con las posiciones solicitadas en la función anterior y le paso la longitud entera del array.
         Library.fillArray(array, arrayPositions, LENGTHMAX);
-        valorMax = Library.ordenarVector(array, LENGTHMAX); //Ordeno el vector y devuelvo valorMax
-        System.out.println("El valor máximo es: " + valorMax);
-        Library.showVector(array, LENGTHMAX); //Muestro el vector
-        media = Library.calcularMedia(array, LENGTHMAX);
+        Library.showVector(array, 3); //Recorro el vector tantas posiciones como indique el parametro y voy imprimiendo sus valores. 
+                                      //Por ejemplo, que me muestre sólo las 3 primeras posiciones.
+
+        valorMax = Library.ordenarVector(array, 3); //Ordeno el vector hasta el parametro indicado y devuelvo valorMax de entre esas posiciones
+        System.out.println("\nEl valor máximo hasta la posicion indicada es: " + valorMax);
+
+        System.out.println("");
+        valorMax= Library.ordenarVector(array, LENGTHMAX);//Ahora quiero ordenar todos los elementos del vector.
+         Library.showVector(array, LENGTHMAX); //Como hemos ordenado el vector entero, quiero verlo entero ordenado.
+        System.out.println("\nEl valor máximo de todo el array es: " + valorMax); //Me devuele el valorMax de todo el vector
+        media = Library.calcularMedia(array, LENGTHMAX);//Calculo la media de todos los elementos del array
         System.out.println("\nLa media es: " + media);
-        Library.findElement(array, LENGTHMAX, 99); //Busco el número 99
+        Library.findElement(array, LENGTHMAX, 99); //Busco el número que le indique por parámetro, por ejemplo, el 99.
 
     }
 
-    public static void positionsForArray(int array[], boolean _arrayPositions[]) {
+    public static void positionsForArray(int array[], boolean _arrayPositions[], int _asking) {
 
-        String Error = "ERROR! El dato no respeta la Longitud Máxima.";
-        int positions = 0;
-        boolean on = true;
-        int i = 0;
-        int positionInput = 0;
+        
+        boolean on = true; //activa/desactiva el bucle while
+        int i = 0;//Contador
+        int positionInput = 0; //Posicion introducida por el usuario
 
-        //Solicitamos al usuario cuantas posiciones quiere llenar
-        do {
-            System.out.println("Cuantas posiciones del vector quieres llenar?: ");
-            positions = Teclat.llegirInt();
-            if (positions > LENGTHMAX) {
-                System.out.println(Error);
-            } else if (positions < 0) {
-                System.out.println(Error);
-            } else {
-                on = false; //Una vez solicitadas todas, se apaga.
-            }
-        } while (on); //Hay que apagarlo para salir del bucle
-
-        //Solicitamos las posiciones a llenar
-        do {
-            System.out.println("Indica qué posición quieres llenar: ");
-            positionInput = Teclat.llegirInt();
-            if ((positionInput >= LENGTHMAX) || (positionInput < 0)) {
-                System.out.println(Error);
-            } else {
-                _arrayPositions[positionInput] = true; //Si introducimos la posición 2, pues se guarda un true en la posición 2.
-                //De este modo, luego sólo se llenaran las posiciones true.
-                i++;
-            }
-        } while (i < positions); //Se sale del bucle al haber llenado todas las posiciones solicitadas.
-
+        if (_asking > 0) {
+            //Solicitamos las posiciones a llenar
+            do {
+                System.out.println(i + 1 + ".- " + "Indica qué posición quieres llenar: ");
+                positionInput = Teclat.llegirInt();
+                if ((positionInput >= LENGTHMAX) || (positionInput < 0)) {
+                    System.out.println("ERROR! El dato no respeta las longitudes del vector con las que trabajamos.");
+                } else {
+                    _arrayPositions[positionInput] = true; //Si introducimos la posición 2, pues se guarda un true en la posición 2.
+                    //De este modo, luego sólo se llenaran las posiciones true.
+                    i++;
+                }
+            } while (i < _asking); //Se sale del bucle al haber llenado todas las posiciones solicitadas.
+        }
     }
 }
+
